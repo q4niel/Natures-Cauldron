7125a0ac54c66b21d63d0114352d690598f7551c
q4niel <github.obscure938@passmail.net>
2025-04-15 21:59:20 +0200

    'BrewingStandScreenHandlerMixin' fuel modification with JSON config

--- src/main/kotlin/dev/q4niel/natures_cauldron/ServerConfigFile.kt
+++ src/main/kotlin/dev/q4niel/natures_cauldron/ServerConfigFile.kt
@@ -4,5 +4,6 @@ data class ServerConfigFile (
     var glassBottleMaxCount: Int = 16,
     var potionMaxCount: Int = 16,
     var splashPotionMaxCount: Int = 16,
-    var lingeringPotionMaxCount: Int = 16
+    var lingeringPotionMaxCount: Int = 16,
+    var brewingStandFuelIngredient: String = "minecraft:charcoal"
 )
