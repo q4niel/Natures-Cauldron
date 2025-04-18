5ce832a2307a041ee07ebef06c76cf7b35afd18e
q4niel <github.obscure938@passmail.net>
2025-04-18 19:41:39 +0200

    fixed mixin & brewing stand recipe issues from <- [kt-reimpl-of-0.2.0]

--- src/main/java/dev/q4niel/natures_cauldron/mixin/ItemsMixin.java
+++ src/main/java/dev/q4niel/natures_cauldron/mixin/ItemsMixin.java
@@ -1,73 +1,56 @@
 package dev.q4niel.natures_cauldron.mixin;
 
 import dev.q4niel.natures_cauldron.StaticConfig;
-import dev.q4niel.natures_cauldron.NaturesCauldron;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
-import net.minecraft.registry.RegistryKey;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(Items.class)
 public class ItemsMixin {
 
-    @ModifyArgs (
-            method = "register(Ljava/lang/String;Ljava/util/function/Function;)Lnet/minecraft/item/Item;",
+    @ModifyArg (
+            method = "<clinit>",
             at = @At (
                     value = "INVOKE",
-                    target = "Lnet/minecraft/item/Items;register(Lnet/minecraft/registry/RegistryKey;Ljava/util/function/Function;Lnet/minecraft/item/Item$Settings;)Lnet/minecraft/item/Item;"
+                    target = "Lnet/minecraft/item/GlassBottleItem;<init>(Lnet/minecraft/item/Item$Settings;)V"
             )
     )
-    private static void foo(Args args) {
-        Args mod = register(args);
-        for (int i = 0; i < args.size(); i++) {
-            args.set(i, mod.get(i));
-        }
+    private static Item.Settings GlassBottleItem(Item.Settings settings) {
+        return settings.maxCount(StaticConfig.GLASS_BOTTLE_MAX_COUNT);
     }
 
-    @ModifyArgs (
-            method = "register(Ljava/lang/String;Ljava/util/function/Function;Lnet/minecraft/item/Item$Settings;)Lnet/minecraft/item/Item;",
+    @ModifyArg (
+            method = "<clinit>",
             at = @At (
                     value = "INVOKE",
-                    target = "Lnet/minecraft/item/Items;register(Lnet/minecraft/registry/RegistryKey;Ljava/util/function/Function;Lnet/minecraft/item/Item$Settings;)Lnet/minecraft/item/Item;"
+                    target = "Lnet/minecraft/item/PotionItem;<init>(Lnet/minecraft/item/Item$Settings;)V"
             )
     )
-    private static void bar(Args args) {
-        Args mod = register(args);
-        for (int i = 0; i < args.size(); i++) {
-            args.set(i, mod.get(i));
-        }
+    private static Item.Settings PotionItem(Item.Settings settings) {
+        return settings.maxCount(StaticConfig.POTION_MAX_COUNT);
     }
 
-    private static Item.Settings maxCount(Item.Settings settings, int value) {
-        return settings.maxCount(value);
+    @ModifyArg (
+            method = "<clinit>",
+            at = @At (
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/item/SplashPotionItem;<init>(Lnet/minecraft/item/Item$Settings;)V"
+            )
+    )
+    private static Item.Settings SplashPotionItem(Item.Settings settings) {
+        return settings.maxCount(StaticConfig.SPLASH_POTION_MAX_COUNT);
     }
 
-    private static Args register(Args args) {
-        switch (((RegistryKey)args.get(0)).getValue().toTranslationKey().toString()) {
-            case "minecraft.glass_bottle": {
-                args.set(2, maxCount(args.get(2), StaticConfig.GLASS_BOTTLE_MAX_COUNT));
-                break;
-            }
-
-            case "minecraft.potion": {
-                args.set(2, maxCount(args.get(2), StaticConfig.POTION_MAX_COUNT));
-                break;
-            }
-
-            case "minecraft.splash_potion": {
-                args.set(2, maxCount(args.get(2), StaticConfig.SPLASH_POTION_MAX_COUNT));
-                break;
-            }
-
-            case "minecraft.lingering_potion": {
-                args.set(2, maxCount(args.get(2), StaticConfig.LINGERING_POTION_MAX_COUNT));
-                break;
-            }
-        }
-
-        return args;
+    @ModifyArg (
+            method = "<clinit>",
+            at = @At (
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/item/LingeringPotionItem;<init>(Lnet/minecraft/item/Item$Settings;)V"
+            )
+    )
+    private static Item.Settings LingeringPotionItem(Item.Settings settings) {
+        return settings.maxCount(StaticConfig.LINGERING_POTION_MAX_COUNT);
     }
 }
