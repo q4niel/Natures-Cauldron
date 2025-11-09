72cd75174cd5d7e1382f048d337caa9bcb1c157b
q4niel <github.obscure938@passmail.net>
2025-11-09 14:25:44 +0100

    initialize new fabric mod template

--- src/client/kotlin/dev/q4niel/NaturesCauldronClient.kt
+++
@@ -1,9 +0,0 @@
-package dev.q4niel
-
-import net.fabricmc.api.ClientModInitializer
-
-object NaturesCauldronClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
